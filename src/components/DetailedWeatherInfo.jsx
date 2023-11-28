e3ad9f3b9de60c48a2a91dce28c407ee27bed4f3
Juliana <julianapereira.dev@gmail.com>
2023-11-28 17:24:34 -0300

    style: Desktop more responsive with rezise

--- src/components/DetailedWeatherInfo.jsx
+++ src/components/DetailedWeatherInfo.jsx
@@ -18,14 +18,79 @@ export default function DetailedWeatherInfo(props) {
 
   return (
     <RightContainer>
-      <h1>DetailedWeatherInfo</h1>
+      <RightBox>
+
+        <RightHeaderBox>
+          <button>HOJE</button>
+          <button>PRÓXIMOS DIAS</button>
+        </RightHeaderBox>
+
+        <RightMiddleBox>
+          <h2>Nome da Cidade</h2>
+          <h3>Lat e Long</h3>
+
+          <h4>Ternário para saber se é para renderizar os quadrados ou o gráfico</h4>
+          <h5>Componente dos Quadrados</h5>
+          <h5>Componente do Gráfico</h5>
+        </RightMiddleBox>
+
+        <RightBottomBox>
+          <p>Dados fornecidos pela <span>Open Weather API</span></p>
+        </RightBottomBox>
+
+      </RightBox>
     </RightContainer>
   );
 }
 
 const RightContainer = styled.div`
-  width: 100%;
-  max-width: 65%;
+  width: 65%;
   padding: 15px 20px 5px 20px;
-  background-color: #6b0080;
+  background-color: #EDEDED;
+`;
+
+const RightBox = styled.div`
+  width: 100%;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: normal;
+`;
+
+const RightHeaderBox = styled.div`
+  color: #222222;
+  display: flex;
+  align-items: center;
+  margin-bottom: 40px;
+  //background-color: yellow;
+  width: 100%;
+  
+  img {
+    width: 20%;
+    margin-right: 20px;
+  }
+
+  h1 {
+    font-family: 'Poppins', sans-serif;
+    font-size: 3vw;
+    font-weight: 600;
+  }
+`;
+
+const RightMiddleBox = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  //background-color: pink;
+  margin-bottom: 40px;
+  width: 100%;
+  height: 100%;
+`;
+
+
+const RightBottomBox = styled.div`
+  color: #222222;
+  position: absolute;
+  bottom: 0%;
+  //background-color: #a6ff00;
 `;
--- src/components/GeneralWeatherInfo.jsx
+++ src/components/GeneralWeatherInfo.jsx
@@ -1,27 +1,91 @@
 import styled from "styled-components";
+import coat from "../images/coat.png";
 
 export default function GeneralWeatherInfo(props) {
-  
-  const { 
-    setSearch, 
-    unit, 
-    setUnit,
-    temp, 
-    weatherZeroMain, 
-    description, 
-    icon 
-  } = props;
+  const { setSearch, unit, setUnit, temp, weatherZeroMain, description, icon } =
+    props;
 
   return (
     <LeftContainer>
-      <h1>GeneralWeatherInfo</h1>
+      <LeftBox>
+
+        <LeftHeaderBox>
+          <img src={coat} alt="" />
+          <h1>Levo um casaquinho?</h1>
+        </LeftHeaderBox>
+
+        <LeftMiddleBox>
+          <h2>Componente Input de Pesquisa</h2>
+          <h3>Componente com  outras infos</h3>
+
+          <SeparateLine />
+
+          <h4>Componente do dia/horário</h4>
+          <button>Componente Button Switch</button>
+        </LeftMiddleBox>
+
+        <LeftBottomBox>
+          <p>Todos os direitos reservados. 2023.</p>
+        </LeftBottomBox>
+
+      </LeftBox>
     </LeftContainer>
   );
 }
 
 const LeftContainer = styled.div`
-  width: 100%;
-  max-width: 35%;
+  width: 35%;
   padding: 15px 20px 5px 20px;
-  background-color: green;
+  background-color: #FFFFFF;
+`;
+
+const LeftBox = styled.div`
+  width: 100%;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+const LeftHeaderBox = styled.div`
+  color: #222222;
+  display: flex;
+  align-items: center;
+  margin-bottom: 40px;
+  //background-color: yellow;
+  width: 100%;
+  
+  img {
+    width: 20%;
+    margin-right: 20px;
+  }
+
+  h1 {
+    font-family: 'Poppins', sans-serif;
+    font-size: 3vw;
+    font-weight: 600;
+  }
+`;
+
+const LeftMiddleBox = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  //background-color: pink;
+  margin-bottom: 40px;
+  width: 100%;
+  height: 100%;
+`;
+
+const SeparateLine = styled.div`
+  width: 90%;
+  height: 2px;
+  background: #EDEDED;
+`;
+
+const LeftBottomBox = styled.div`
+  color: #222222;
+  position: absolute;
+  bottom: 0%;
+  //background-color: #a6ff00;
 `;
